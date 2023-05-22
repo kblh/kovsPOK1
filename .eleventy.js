43dc024dcc5239485f5517dcc28259034c8190f8
Petr Kolacek <petr.kolacek@etnetera.cz>
2023-05-22 21:27:29 +0200

    EleventyHtmlBasePlugin

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,26 @@
+const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
+
 module.exports = config => {
+  config.addPlugin(EleventyHtmlBasePlugin, {
+    // The base URL: defaults to Path Prefix
+    baseHref: config.pathPrefix,
+
+    // But you could use a full URL here too:
+    // baseHref: "http://example.com/"
+
+    // Comma separated list of output file extensions to apply
+    // our transform to. Use `false` to opt-out of the transform.
+    extensions: "html",
+
+    // Rename the filters
+    filters: {
+      base: "htmlBaseUrl",
+      html: "transformWithHtmlBase",
+      pathPrefix: "/kovs/",
+    },
+  });
+
+
   // Set directories to pass through to the dist folder
   config.addPassthroughCopy('./src/css/');
   config.addPassthroughCopy('./src/css/*');
